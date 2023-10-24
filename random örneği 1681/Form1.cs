6c2f7b86a829f31314a9a538e8694af2412369b0
denizsu34 <148059058+denizsu34@users.noreply.github.com>
2023-10-24 14:36:09 +0300

    Add files via upload

---
+++ random örneği 1681/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace random_örneği_1681
+{ 
+    public partial class btnUret : Form
+    {
+        public btnUret()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           lbNotlar.Items.Clear();
+            Random rnd = new Random();
+
+            for(int i = 0; i < 15; i++)
+            {
+                int puam = rnd.Next(0,100);
+                lbNotlar.Items.Add(puam);
+            }
+            
+            {
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbNotlar.Items.Clear();
+            Random rnd = new Random();
+
+            for (int i = 0; i < 15; i++)
+            {
+                int puam = rnd.Next(0, 100);
+                lbNotlar.Items.Add(puam);
+            }
+        }
+    }
+}
